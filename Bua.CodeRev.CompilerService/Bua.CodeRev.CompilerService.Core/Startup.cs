53a4c6f088281d2f61d471e4967cf3c59d00d571
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-04-10 18:30:29 +0500

    cors

--- Bua.CodeRev.CompilerService/Bua.CodeRev.CompilerService.Core/Startup.cs
+++ Bua.CodeRev.CompilerService/Bua.CodeRev.CompilerService.Core/Startup.cs
@@ -20,7 +20,7 @@ namespace Bua.CodeRev.CompilerService.Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -45,6 +45,11 @@ namespace Bua.CodeRev.CompilerService.Core
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
 
             app.UseEndpoints(endpoints =>
             {
