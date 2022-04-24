70c885b71b50ced12547e8e80cd4adc4e84cd944
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-04-25 00:59:59 +0500

    code controls component

---
+++ Bua.CodeRev.WebClient/src/app/code-editor/components/controls/controls.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
+import { EntryPoint } from 'src/app/code-editor/models/entryPoint';
+import { CodeStorageService } from 'src/app/code-editor/services/code-storage-service/code-storage.service';
+import { CompileService } from 'src/app/code-editor/services/compile-service/compile-service.service';
+import { RecordService } from 'src/app/code-editor/services/record-service/record.service';
+
+@Component({
+    selector: 'app-controls',
+    templateUrl: './controls.component.html',
+    styleUrls: ['./controls.component.less']
+})
+export class ControlsComponent {
+    private _bindedEditor?: CodemirrorComponent;
+
+    constructor(
+        private _compiler: CompileService,
+        private _record: RecordService,
+        private _codeStorage: CodeStorageService) { }
+
+    public bindToEditor(editor: CodemirrorComponent): void {
+        this._bindedEditor = editor;
+    }
+    
+    public saveAndRun(): void {
+        this._codeStorage.saveCode(this._bindedEditor?.codeMirror?.getValue() ?? '');
+
+        this._compiler
+            .execute(
+                this._codeStorage.getSavedCode(),
+                new EntryPoint(
+                    this._codeStorage.entryNamespace, 
+                    this._codeStorage.entryClass, 
+                    this._codeStorage.entryMethod))
+            .subscribe(result => 
+                this._codeStorage.storeOutput(result));
+    }
+
+    public startRecord(): void {
+        if (!this._bindedEditor) {
+            console.log('Controls not binded to any exist editor');
+
+            return;
+        }
+
+        this._record.startRecord(this._bindedEditor);
+    }
+
+    public stopAndSaveRecord(): void {
+        if (!this._bindedEditor) {
+            console.log('Controls not binded to any exist editor');
+
+            return;
+        }
+
+        this._record.stopAndSaveRecord(this._bindedEditor);
+    }
+
+    public playSavedRecord(): void {
+        if (!this._bindedEditor) {
+            console.log('Controls not binded to any exist editor');
+
+            return;
+        }
+
+        this._record.playSavedRecord(this._bindedEditor);
+    }
+
+}
