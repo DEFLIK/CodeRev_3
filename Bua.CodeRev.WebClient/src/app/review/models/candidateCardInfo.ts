3551b54f070549dc014a5d1e61d2842af4ea4e4a
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-01 02:30:37 +0500

    cardInfo model

---
+++ Bua.CodeRev.WebClient/src/app/review/models/candidateCardInfo.ts
@@ -0,0 +1,10 @@
+export class CandidateCardInfo {
+    public userId!: string;
+    public fullName!: string;
+    public vacancy!: string;
+    public startTimeMs!: number;
+    public timeToCheckMs!: number;
+    public averageGrade!: number;
+    public doneTasksCount!: number;
+    public tasksCount!: number;
+}
