7e16b272fc81616fa980885833989cb607578e4d
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-01 16:43:40 +0500

    card info upd + getState method

--- Bua.CodeRev.WebClient/src/app/review/models/candidateCardInfo.ts
+++ Bua.CodeRev.WebClient/src/app/review/models/candidateCardInfo.ts
@@ -1,10 +1,30 @@
+import { CandidateState } from './candidateState';
+
 export class CandidateCardInfo {
     public userId!: string;
+    public interviewSolutionId!: string;
     public fullName!: string;
     public vacancy!: string;
     public startTimeMs!: number;
     public timeToCheckMs!: number;
     public averageGrade!: number;
+    public reviewerComment!: string;
     public doneTasksCount!: number;
     public tasksCount!: number;
+
+    public getState(): CandidateState {
+        if (this.tasksCount === this.doneTasksCount && this.startTimeMs !== -1 && Date.now() < this.timeToCheckMs) {
+            return CandidateState.done;
+        }
+
+        if (this.tasksCount > this.doneTasksCount && this.startTimeMs !== -1 && Date.now() < this.timeToCheckMs) {
+            return CandidateState.inProcess;
+        }
+
+        if (this.startTimeMs === -1 && Date.now() < this.timeToCheckMs) {
+            return CandidateState.notStarted;
+        }
+
+        return CandidateState.skiped;
+    }
 }
