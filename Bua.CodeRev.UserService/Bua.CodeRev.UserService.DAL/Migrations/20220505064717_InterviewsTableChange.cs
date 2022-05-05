a562c17d123fbfaf9d2a0c952badf39cd66e9384
kiD-9 <d.kirill.ekb@gmail.com>
2022-05-05 12:44:06 +0500

    DAL migrations

---
+++ Bua.CodeRev.UserService/Bua.CodeRev.UserService.DAL/Migrations/20220505064717_InterviewsTableChange.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bua.CodeRev.UserService.DAL.Migrations
+{
+    public partial class InterviewsTableChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "InterviewDurationMs",
+                table: "Interviews",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InterviewDurationMs",
+                table: "Interviews");
+        }
+    }
+}
