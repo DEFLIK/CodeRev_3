7e5a4325b78a2da8f634d3d55883eb1489e9147b
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-05 23:21:19 +0500

    candidate grade component logic & layout

--- Bua.CodeRev.WebClient/src/app/review/components/candidate-grade/candidate-grade.component.ts
+++ Bua.CodeRev.WebClient/src/app/review/components/candidate-grade/candidate-grade.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CandidateCardInfo } from '../../models/candidateCardInfo';
+type FormControlType = { [key: string]: AbstractControl };
 
 @Component({
     selector: 'app-candidate-grade',
@@ -10,12 +13,32 @@ export class CandidateGradeComponent implements OnInit {
     public get id(): string {
         return this._solutionId;
     }
+    public get formControls(): FormControlType {
+        const res: FormControlType = {
+            resultComment: new FormControl(''),
+            resultGrade: new FormControl(-1)
+        };
+        for (const task of this.tasks) {
+            res[`${task}`] = new FormControl(-1);
+        }
+
+        return res;
+    }
+    public tasks = ['A','B','C','D','E','F','G'];
+    public gradesForm: FormGroup = new FormGroup(this.formControls);
+    public candidate!: CandidateCardInfo;
     private _solutionId!: string;
 
-    constructor(private _activatedRoute: ActivatedRoute) { }
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _router: Router
+    ) { }
 
     public ngOnInit(): void {
         this._solutionId = this._activatedRoute.snapshot.paramMap.get('solutionId') ?? '';
     }
 
+    public log(): void {
+        console.log(this.gradesForm);
+    }
 }
