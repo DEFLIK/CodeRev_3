4a510d91a98ed35b5a234d1450fa5b8570c9edff
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-07 01:09:42 +0500

    auth interceptor

---
+++ Bua.CodeRev.WebClient/src/app/auth/interceptor/authInterceptor.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SessionStorageService } from '../services/sessionStorage-service/session-storage.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private _cacher: SessionStorageService) {}
+
+    public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const sessionToken: string = this._cacher.getSession().token;
+
+        if (sessionToken) {
+            const newRequest: HttpRequest<any> = req.clone({
+                headers: req.headers.set('authorization', sessionToken)
+            });
+
+            return next.handle(newRequest);
+        }
+
+        return next.handle(req);
+    }
+}
