f02dda633f040f0770bc3fc36b8c541de842d662
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-07 01:10:05 +0500

    session storage service

--- Bua.CodeRev.WebClient/src/app/auth/services/sessionStorage-service/session-storage.service.ts
+++ Bua.CodeRev.WebClient/src/app/auth/services/sessionStorage-service/session-storage.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { ISession } from '../../models/iSession';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SessionStorageService {
+    private _sessionIdentifier: string = 'session';
 
-    constructor() { }
+    constructor() {}
+
+    public cacheSession(session: ISession): void {
+        localStorage.setItem(this._sessionIdentifier, JSON.stringify(session));
+    }
+
+    public removeSession(): ISession {
+        var empty: ISession = { token: '' };
+        this.cacheSession(empty);
+
+        return empty;
+    }
+
+    public getSession(): ISession {
+        var session: string | null = localStorage.getItem(this._sessionIdentifier);
+
+        if (session === null) {
+            return this.removeSession();
+        }
+
+        return (JSON.parse(session) as ISession); 
+    }
 }
