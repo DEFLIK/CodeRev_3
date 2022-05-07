9190bb18167ae307b2543cdc57bf902b83e87617
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-07 19:10:44 +0500

    session storage upd

--- Bua.CodeRev.WebClient/src/app/auth/services/sessionStorage-service/session-storage.service.ts
+++ Bua.CodeRev.WebClient/src/app/auth/services/sessionStorage-service/session-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ISession } from '../../models/iSession';
+import { IJWTSession } from '../../models/iJwtSession';
+import { UserSession } from '../../models/iUserSession';
 
 @Injectable({
     providedIn: 'root'
@@ -9,24 +10,48 @@ export class SessionStorageService {
 
     constructor() {}
 
-    public cacheSession(session: ISession): void {
+    public cacheJWTSession(session: IJWTSession): void {
         localStorage.setItem(this._sessionIdentifier, JSON.stringify(session));
     }
 
-    public removeSession(): ISession {
-        var empty: ISession = { token: '' };
-        this.cacheSession(empty);
+    public removeJWTSession(): IJWTSession {
+        var empty: IJWTSession = { accessToken: '' };
+        this.cacheJWTSession(empty);
 
         return empty;
     }
 
-    public getSession(): ISession {
+    public getJWTSession(): IJWTSession {
         var session: string | null = localStorage.getItem(this._sessionIdentifier);
 
         if (session === null) {
-            return this.removeSession();
+            return this.removeJWTSession();
         }
 
-        return (JSON.parse(session) as ISession); 
+        return (JSON.parse(session) as IJWTSession); 
     }
+
+    public getJWTInfo(): UserSession {
+        const jwtSession = this.getJWTSession();
+
+        if (!jwtSession || !jwtSession.accessToken) {
+            return new UserSession();
+        }
+
+        const newObj = this.parseJwt(jwtSession.accessToken);
+        const session = new UserSession;
+        Object.assign(session, newObj);
+
+        return session;
+    }
+
+    public parseJwt(token: string): object {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
+    };
 }
