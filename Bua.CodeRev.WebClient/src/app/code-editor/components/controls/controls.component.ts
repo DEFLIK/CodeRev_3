870a380065e2a322689c1805496eba4c572a85e1
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-29 01:16:19 +0500

    controls time line logic upd

--- Bua.CodeRev.WebClient/src/app/code-editor/components/controls/controls.component.ts
+++ Bua.CodeRev.WebClient/src/app/code-editor/components/controls/controls.component.ts
@@ -2,7 +2,7 @@ import { AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, Co
 import { FormControl, FormGroup } from '@angular/forms';
 import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
 import { CanvasPos, NgxVideoTimelineComponent, VideoCellType } from 'ngx-video-timeline';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 import { EntryPoint } from 'src/app/code-editor/models/entryPoint';
 import { CodeStorageService } from 'src/app/code-editor/services/code-storage-service/code-storage.service';
 import { CompileService } from 'src/app/code-editor/services/compile-service/compile-service.service';
@@ -10,6 +10,7 @@ import { RecordService } from 'src/app/code-editor/services/record-service/recor
 import { IOperationMark, RecordInfo } from '../../models/codeRecord';
 import { EditorMode } from '../../models/editorMode';
 import { ExecutionResult } from '../../models/executionResult';
+import { ExecutionResultResponse } from '../../models/response/executionResult-response';
 import { PatchedTimelineComponent } from '../patched-timeline/patched-timeline.component';
 
 @Component({
@@ -27,6 +28,8 @@ export class ControlsComponent implements OnInit, OnDestroy {
     public patchedTimeline!: PatchedTimelineComponent;
     @Input()
     public editorMode!: EditorMode;
+    @Input()
+    public taskSelected$!: Observable<string>;
     public startTime: number = new Date().getTime();
     public get isPlaying(): boolean {
         return this._record.isPlaying;
@@ -50,58 +53,83 @@ export class ControlsComponent implements OnInit, OnDestroy {
         slider: new FormControl('')
     });
     private _bindedEditor?: CodemirrorComponent;
-    private _rangeUpdater$?: Subscription;
+    private _rangeUpdater?: Subscription;
+    private _currentTask?: string;
+    private _taskUpdater?: Subscription;
 
     constructor(
         private _compiler: CompileService,
         private _record: RecordService,
-        private _codeStorage: CodeStorageService
+        private _codeStorage: CodeStorageService,
     ) {
-        if (this.editorMode === EditorMode.review) {
-            this._rangeUpdater$ = interval(100)
-                .subscribe(() => {
-                    const currentTime = _record.getCurrentTime();
-                    if (this._record.isPlaying && currentTime >= 0) {
-                        this.inputForm.get('slider')?.disable();
-                        this.inputForm.get('slider')?.setValue(currentTime);
-                    } else {
-                        this.inputForm.get('slider')?.enable();
-                    }
-                });
-        }
     }
 
     public ngOnDestroy(): void {
-        this._rangeUpdater$?.unsubscribe();
+        this._rangeUpdater?.unsubscribe();
+        this._taskUpdater?.unsubscribe();
     }
     public ngOnInit(): void {
         this.inputForm.get('slider')?.setValue(0);
+
+        this._taskUpdater = this.taskSelected$
+            .subscribe(task => {
+                this._currentTask = task;
+
+                if (this.editorMode === EditorMode.write) {
+                    this._record.setRecordingTask(task);
+                }
+
+                if (this.editorMode === EditorMode.review) {
+                    this._record.selectTaskRecord(task);
+                    this.patchedTimeline.setProperties(
+                        this.startTime, 
+                        this._record.getDuration(), 
+                        this._record.getTaskRecord(task));
+
+                    this._rangeUpdater = interval(100)
+                        .subscribe(() => {
+                            const currentTime = this._record.getCurrentTime();
+                            
+                            if (this._record.isPlaying && currentTime >= 0) {
+                                this.inputForm.get('slider')?.disable();
+                                this.inputForm.get('slider')?.setValue(currentTime);
+                                // this.patchedTimeline.timeLineComp!.isPlayClick = true;
+                                this.patchedTimeline.setCurrentTime(currentTime);
+                            } else {
+                                this.inputForm.get('slider')?.enable();
+                                // this.patchedTimeline.timeLineComp!.isPlayClick = false;
+                            }
+                        });
+                }
+            });
     }
 
     public bindToEditor(editor: CodemirrorComponent): void {
         this._bindedEditor = editor;
         this._record.bindEditor(editor, this.editorMode);
-        this.patchedTimeline.buildComponent();
-        this.patchedTimeline.setProperties(this.startTime, this._record.getDuration(), this._record.getRecords());
+
+        if (this.editorMode === EditorMode.review) {
+            this.patchedTimeline.buildComponent();
+        }
     }
 
-    public change(): void {
-        this.patchedTimeline.setProperties(new Date().getTime(), this._record.getDuration(), new RecordInfo([]));
-    }
-    
     public saveAndRun(): void {
-        this._codeStorage.saveCode(this._bindedEditor?.codeMirror?.getValue() ?? '');
+        if (!this._currentTask) {
+            return;
+        }
+
+        this._codeStorage.saveCode(this._currentTask, this._bindedEditor?.codeMirror?.getValue() ?? '');
 
         this._compiler
             .execute(
-                this._codeStorage.getSavedCode(),
+                this._codeStorage.getSavedCode(this._currentTask),
                 new EntryPoint(
                     this._codeStorage.entryNamespace, 
                     this._codeStorage.entryClass, 
                     this._codeStorage.entryMethod))
             .subscribe(resp => {
                 if (resp.ok) {
-                    this._codeStorage.storeOutput(resp.body ?? new ExecutionResult());
+                    this._codeStorage.storeOutput(resp.body ?? new ExecutionResult(new ExecutionResultResponse()));
                 } else {
                     //todo
                 }
@@ -109,48 +137,38 @@ export class ControlsComponent implements OnInit, OnDestroy {
     }
 
     public startRecord(): void {
-        if (!this._bindedEditor) {
-            console.log('Controls not binded to any exist editor');
-
+        if (!this._bindedEditor || !this._currentTask) {
             return;
         }
 
-        this._record.startRecord();
+        this._record.startTaskRecord(this._currentTask);
     }
 
     public stopAndSaveRecord(): void {
-        if (!this._bindedEditor) {
-            console.log('Controls not binded to any exist editor');
-
+        if (!this._bindedEditor || !this._currentTask) {
             return;
         }
 
-        this._record.stopAndSaveRecord();
+        this._record.stopAndSaveTaskRecord(this._currentTask);
     }
 
     public playSavedRecord(): void {
-        if (!this._bindedEditor) {
-            console.log('Controls not binded to any exist editor');
-
+        if (!this._bindedEditor || !this._currentTask) {
             return;
         }
 
-        this._record.playSavedRecord();
+        this._record.play();
     }
 
     public pauseSavedRecord(): void {
-        if (!this._bindedEditor) {
-            console.log('Controls not binded to any exist editor');
-
+        if (!this._bindedEditor || !this._currentTask) {
             return;
         }
 
-        this._record.pauseSavedRecord();
+        this._record.pause();
     }
 
     public seek(time: number): void {
-        // this._record.seek(this.inputForm.get('slider')?.value);
-        console.log(time- this.startTime);
         this._record.seek(time - this.startTime);
     }
 
