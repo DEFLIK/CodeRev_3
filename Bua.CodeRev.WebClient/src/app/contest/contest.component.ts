cc4574a81ef172b1bb04f9acb443ca021cfc4a6f
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-29 01:21:33 +0500

    task selection contest component

--- Bua.CodeRev.WebClient/src/app/contest/contest.component.ts
+++ Bua.CodeRev.WebClient/src/app/contest/contest.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { EditorMode } from '../code-editor/models/editorMode'; 
 
 @Component({
     selector: 'app-contest',
     templateUrl: './contest.component.html',
     styleUrls: ['./contest.component.less']
 })
-export class ContestComponent implements OnInit {
-
+export class ContestComponent {
+    public types = EditorMode;
+    public get taskSelected$(): Observable<string> {
+        return this._taskSelected$.asObservable();
+    }
+    public get isTaskSelected(): boolean {
+        return !!this._currentTask;
+    }
+    public tasks = ['task1', 'task2'];
+    private _currentTask?: string;
+    private _taskSelected$ = new Subject<string>();
     constructor() { }
 
-    ngOnInit(): void {
+    public change(task: string): void {
+        this._taskSelected$.next(task);
     }
 
 }
