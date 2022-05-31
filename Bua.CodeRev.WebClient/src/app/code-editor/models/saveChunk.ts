5cee538da85b41f9033beb4b143aa99dcdcf6278
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-31 19:33:12 +0500

    saveChunk model

---
+++ Bua.CodeRev.WebClient/src/app/code-editor/models/saveChunk.ts
@@ -0,0 +1,10 @@
+import { RecordInfo } from './codeRecord';
+
+export class SaveChunk {
+    constructor(
+        public taskId: string,
+        public saveTime: number,
+        public code: string,
+        public record: RecordInfo
+    ) {}
+}
