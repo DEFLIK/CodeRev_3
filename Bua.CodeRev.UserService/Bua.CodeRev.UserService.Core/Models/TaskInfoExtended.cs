043944fd45b3eb1867cd2d71e72eb3d11fe48fe5
kiD-9 <d.kirill.ekb@gmail.com>
2022-06-03 17:25:00 +0500

    task info extended class for api

---
+++ Bua.CodeRev.UserService/Bua.CodeRev.UserService.Core/Models/TaskInfoExtended.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bua.CodeRev.UserService.Core.Models
+{
+    public class TaskInfoExtended
+    {
+        public Guid TaskSolutionId { get; set; }
+        public Guid TaskId { get; set; }
+        public char TaskOrder;
+        public string TaskText { get; set; }
+        public string StartCode { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
