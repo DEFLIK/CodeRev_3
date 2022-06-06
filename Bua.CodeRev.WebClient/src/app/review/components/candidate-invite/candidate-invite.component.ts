09b0137ee9db704685190ad1dcb67888f63a81cb
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-06-06 12:19:45 +0500

    invite component

---
+++ Bua.CodeRev.WebClient/src/app/review/components/candidate-invite/candidate-invite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-candidate-invite',
+    templateUrl: './candidate-invite.component.html',
+    styleUrls: ['./candidate-invite.component.less']
+})
+export class CandidateInviteComponent {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
