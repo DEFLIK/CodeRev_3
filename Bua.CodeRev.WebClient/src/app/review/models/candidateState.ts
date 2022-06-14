defcac4a93af7a66455d60b5013a1805ff170898
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-06-14 16:41:02 +0500

    task upd

--- Bua.CodeRev.WebClient/src/app/review/models/candidateState.ts
+++ Bua.CodeRev.WebClient/src/app/review/models/candidateState.ts
@@ -2,7 +2,7 @@ export enum CandidateState {
     'done' = 'Выполнено',
     'inProcess' = 'В просессе',
     'notStarted' = 'Не начато',
-    'skiped' = 'Не выполнено'
+    'skiped' = 'Истёк срок'
 }
 
 export type CandidateStateKeys = keyof typeof CandidateState
