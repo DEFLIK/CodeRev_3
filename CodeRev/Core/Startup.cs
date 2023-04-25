99d0fad7aad630b81bf59d1a27a5c0f4d4fa8c1f
kiD-9 <d.kirill.ekb@gmail.com>
2023-04-25 10:59:31 +0500

    Автоприменение миграций для базы при запуске Core

--- CodeRev/Core/Startup.cs
+++ CodeRev/Core/Startup.cs
@@ -61,8 +61,10 @@ namespace Core
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
+            context.Database.Migrate();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
