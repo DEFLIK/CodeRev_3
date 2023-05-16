2a78bc3bdd99b3abac2aa30f4e8f0058445d44bc
kiD-9 <d.kirill.ekb@gmail.com>
2023-05-17 00:53:51 +0500

    Количество попыток запустить код на фронте

--- CodeRev/WebClient/src/app/contest/services/contest.service.ts
+++ CodeRev/WebClient/src/app/contest/services/contest.service.ts
@@ -1,6 +1,6 @@
 ﻿import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { audit, map, Observable, Subject, tap } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { HttpService } from 'src/app/global-services/request/http.service';
 import { RequestMethodType } from 'src/app/global-services/request/models/request-method';
 import { UrlRoutes } from 'src/app/global-services/request/models/url-routes';
@@ -109,6 +109,14 @@ export class ContestService {
         return this._http.request<LastSavedCodeResponse>({
             url: `${UrlRoutes.tracker}/api/v1.0/tracker/get-last-code?taskSolutionId=${taskSlnId}`,
             method: RequestMethodType.get,
+            auth: true
+        });
+    }
+
+    public reduceTaskSolutionAttempt(taskSlnId: string): Observable<HttpResponse<number>> {
+        return this._http.request<number>({
+            url: `${UrlRoutes.user}/api/tasks/solution/reduce-attempt?id=${taskSlnId}`,
+            method: RequestMethodType.post,
             auth: true
         });
     }
