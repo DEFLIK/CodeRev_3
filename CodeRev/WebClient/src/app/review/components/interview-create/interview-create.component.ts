d6bf80ee62702370d7e35baf1c7074af40d9c605
kiD-9 <d.kirill.ekb@gmail.com>
2023-05-25 21:14:32 +0500

    Убрал ненужные юзинги и ругонь линтера

--- CodeRev/WebClient/src/app/review/components/interview-create/interview-create.component.ts
+++ CodeRev/WebClient/src/app/review/components/interview-create/interview-create.component.ts
@@ -15,7 +15,7 @@ import { ReviewService } from '../../services/review.service';
 })
 export class InterviewCreateComponent{
     public editor: HTMLElement | null = document.getElementById('codeEdtior');
-    @ViewChild('codeMirror') 
+    @ViewChild('codeMirror')
     public codeMirrorCmpt!: CodemirrorComponent;
     public vacancies: any[] = [];
     public duration: string = '10:00';
