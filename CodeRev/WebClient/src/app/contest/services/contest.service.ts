e391e022de122c3a7c41fb13b8cd1f0c0183ad6c
kiD-9 <d.kirill.ekb@gmail.com>
2023-06-07 18:05:58 +0500

    Выпилил попытки запуска кода

--- CodeRev/WebClient/src/app/contest/services/contest.service.ts
+++ CodeRev/WebClient/src/app/contest/services/contest.service.ts
@@ -9,7 +9,6 @@ import { InterviewSolutionInfoResposne } from '../models/response/interviewSolut
 import { LastSavedCodeResponse } from '../models/response/lastSavedCode-response';
 import { TaskSolutionInfoResponse } from '../models/response/taskSolutionInfo-response';
 import { TaskSolutionInfo } from '../models/taskSolutionInfo';
-import { RunAttemptsLeftResponse } from "../models/response/runAttemptsLeft-response";
 
 @Injectable({
     providedIn: 'root'
@@ -110,14 +109,6 @@ export class ContestService {
         return this._http.request<LastSavedCodeResponse>({
             url: `${UrlRoutes.tracker}/api/v1.0/tracker/get-last-code?taskSolutionId=${taskSlnId}`,
             method: RequestMethodType.get,
-            auth: true
-        });
-    }
-
-    public reduceTaskSolutionAttempt(taskSlnId: string): Observable<HttpResponse<RunAttemptsLeftResponse>> {
-        return this._http.request<RunAttemptsLeftResponse>({
-            url: `${UrlRoutes.user}/api/tasks/solution/reduce-attempt?id=${taskSlnId}`,
-            method: RequestMethodType.post,
             auth: true
         });
     }
